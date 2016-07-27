33dea3c4c54549c5fd56ad2750248610ab9cee83
Robert Myers <rbmyr8@gmail.com>
2016-07-27 11:56:52 -0400

    add comparison to pg-live-select

---
+++ compare/test.js
@@ -0,0 +1,81 @@
+const LivePg = require('pg-live-select');
+const Pool   = require('pg').Pool;
+
+let settings = {
+	"host"     : "localhost",
+	"port"     : 5432,
+	"user"     : "postgres",
+	"database" : "test",
+	"password" : "password"
+};
+
+let liveDb = new LivePg(settings, 'myapp');
+let pool   = new Pool(settings);
+
+let t = null;
+
+(function update() {
+	t = Date.now();
+	pool.connect((error, client, done) => {
+		client.query('update foo set n = n + 1 where id % 1000 = 0', (error, result) => {
+			done();
+			setTimeout(update, 1000);
+		});
+	});
+}());
+
+liveDb.select(`
+	SELECT
+		greatest(1, 2, 3),
+		foo.*,
+		CONCAT(concat('a', 'b'), 'bar') as blah
+	FROM
+		foo
+	WHERE
+		id % 100 = 0
+`).on('update', function(diff, data) {
+	if(t) {
+		console.log(Date.now() - t);
+		t = null;
+	}
+	console.log(data.length);
+});
+
+// pool.connect((error, client, done) => {
+// 	let watcher = new QueryWatcher(client);
+
+// 	let sql = `
+// 		SELECT
+// 			greatest(1, 2, 3),
+// 			foo.*,
+// 			CONCAT(concat('a', 'b'), 'bar') as blah
+// 		FROM
+// 			foo
+// 	`;
+
+// 	// Use this to compare against just running the query
+// 	(function update() {
+// 		console.time('q');
+// 		client.query(sql, (error, result) => {
+// 			console.timeEnd('q');
+// 			setTimeout(update, 100);
+// 		});
+// 	}());
+
+// 	// watcher.watch(sql).then((tracker) => {
+// 	// 	(function update() {
+// 	// 		console.time('qw');
+
+// 	// 		tracker.update().then((r) => {
+// 	// 			console.timeEnd('qw');
+// 	// 			console.log(r.length);
+
+// 	// 			setTimeout(update, 100);
+// 	// 		}, (e) => {
+// 	// 			console.log(e);
+// 	// 		});
+// 	// 	}());
+// 	// }, (e) => {
+// 	// 	console.log(e);
+// 	// });
+// });
